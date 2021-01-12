7e6bccbb2eaa3179ead43ebb3c6f6c17d3c1b2e0
Luis Rangel <luisrangelc@gmail.com>
2021-01-11 21:57:27 -0600

    initial commit

---
+++ apps/instacool-front/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  displayName: 'instacool-front',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': [
+      'babel-jest',
+      { cwd: __dirname, configFile: './babel-jest.config.json' },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/instacool-front',
+};
